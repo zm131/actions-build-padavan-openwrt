53b1332249286c7cc6859abdbacf93a887ca599f
zm131 <53132876+zm131@users.noreply.github.com>
2020-04-27 08:47:40 +0800

    Update DIR-882-5.0.sh

--- padavan/DIR-882-5.0/DIR-882-5.0.sh
+++ padavan/DIR-882-5.0/DIR-882-5.0.sh
@@ -12,7 +12,7 @@ echo "CONFIG_FIRMWARE_CPU_900MHZ=y" >> .config
 
 
 # 科学
-echo "CONFIG_FIRMWARE_INCLUDE_V2RAY=y" >> .config        # 集成v2ray执行文件（3.8M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
+echo "CONFIG_FIRMWARE_INCLUDE_V2RAY=n" >> .config        # 集成v2ray执行文件（3.8M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
 echo "CONFIG_FIRMWARE_INCLUDE_TROJAN=n" >> .config       # 集成trojan执行文件(1.1M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
 
 # 文件
